027cce6b5df651f971e7d7bb4965fda816cd51f8
wishwashan <ravindu.18@cse.mrt.ac.lk>
2021-10-12 14:59:31 +0530

    create db in heroku

--- config/database.js
+++ config/database.js
@@ -2,11 +2,24 @@ const config = require('./config');
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize("inventoryManagement", "root", "", {
-    dialect:"mysql",
-    host:"localhost",
-    logging:console.log
-  });
+// const sequelize = new Sequelize("inventoryManagement", "root", "", {
+//     dialect:"mysql",
+//     host:"localhost",
+//     logging:console.log
+//   });
+
+const sequelize = new Sequelize("heroku_2f094f4ccd51bd3", "b5f38ad8090f87", "fabca632", {
+  dialect:"mysql",
+  host:"us-cdbr-east-04.cleardb.com",
+  logging:console.log
+});
 
 
 module.exports = sequelize;
+
+// mysql://b5f38ad8090f87:fabca632@us-cdbr-east-04.cleardb.com/heroku_2f094f4ccd51bd3?
+
+// username = b5f38ad8090f87
+// psw = fabca632
+// host = us-cdbr-east-04.cleardb.com
+// dbname = heroku_2f094f4ccd51bd3
