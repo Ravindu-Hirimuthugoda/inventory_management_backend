ffd9a7febb2b085f00341ac78ea614ed77442e68
Uditha <ui25.isuranga@gmail.com>
2021-11-04 22:27:42 +0530

    update authentication

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -5,7 +5,9 @@ const bcrypt = require("bcrypt");
 const { successMessage, errorMessage } = require("../utils/response_message");
 const UserService = require('../services/user');
 
-let guest = new UserService();
+
+
+let userService = new UserService();
 
 const login = async (req, res, next) => {
  
@@ -14,11 +16,18 @@ const login = async (req, res, next) => {
         const email = req.body.email;
         const password = req.body.password;        
         let user;
+        let role;
         try {
-            user = await guest.getUser(email);
+            user = await userService.getUser(email);
+            role =  await userService.readUserRole(user.id, user.type.toString());
+            if(role == null){
+                return errorMessage(res, 'Invalid email or password', 401);
+            }
+            console.log(user);
+            console.log(role);
             const validPassword = await bcrypt.compare(password, user.password);
             if(validPassword){              
-                jwt.sign({ userID: user.id, expiresIn: 3600, type:user.type},"secretKey",(err,token)=>{
+                jwt.sign({ userID: role.id, expiresIn: 3600, type:user.type, firstName: role.firstName, lastName: role.lastName},"secretKey",(err,token)=>{
                         if (err) throw err;
                         return successMessage(res, {token})
                 });
